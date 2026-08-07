e3151e1c1ae03fda10c6d2fdc43e8ae3dfeb4265
uZR34l <fabiogitano@hotmail.com>
2026-08-07 04:46:03 +0200

    Update shift_epg.py

--- shift_epg.py
+++ shift_epg.py
@@ -10,8 +10,6 @@ EPG_URL = "https://xmltvfr.fr/xmltv/xmltv.xml"
 PARIS = ZoneInfo("Europe/Paris")
 UTC = ZoneInfo("UTC")
 
-DAYS_TO_KEEP = 2
-
 INPUT = "original.xml"
 
 # Téléchargement
@@ -30,11 +28,77 @@ with urllib.request.urlopen(req) as response:
 tree = ET.parse(INPUT)
 root = tree.getroot()
 
+
 now = datetime.now()
 start_limit = now - timedelta(days=2)
 end_limit = now + timedelta(days=2)
 
 
+def apply_shift(dt):
+    utc_dt = dt.replace(tzinfo=UTC)
+    offset = utc_dt.astimezone(PARIS).utcoffset()
+    return dt + offset
+
+
+for programme in list(root.findall("programme")):
+
+    start = programme.attrib.get("start")
+
+    if start:
+
+        dt = datetime.strptime(
+            start[:14],
+            "%Y%m%d%H%M%S"
+        )
+
+        dt = apply_shift(dt)
+
+        if dt < start_limit or dt > end_limit:
+            root.remove(programme)
+            continue
+
+        programme.attrib["start"] = (
+            dt.strftime("%Y%m%d%H%M%S")
+            + start[14:]
+        )
+
+
+        stop = programme.attrib.get("stop")
+
+        if stop:
+
+            stop_dt = datetime.strptime(
+                stop[:14],
+                "%Y%m%d%H%M%S"
+            )
+
+            stop_dt = apply_shift(stop_dt)
+
+            programme.attrib["stop"] = (
+                stop_dt.strftime("%Y%m%d%H%M%S")
+                + stop[14:]
+            )
+
+
+xml_data = ET.tostring(
+    root,
+    encoding="utf-8"
+)
+
+with gzip.open(
+    "epg.xml.gz",
+    "wb"
+) as f:
+
+    f.write(
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+    )
+    f.write(xml_data)
+
+
+os.remove(INPUT)
+
+print("EPG corrigé généré avec gestion automatique été/hiver")
 def apply_shift(dt):
     """
     Ajoute automatiquement +1h ou +2h selon l'heure française
